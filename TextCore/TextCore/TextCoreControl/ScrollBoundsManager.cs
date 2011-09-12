340828f07491e817aaff8d9d2d2c14f36c91e2ee
sushanth_r <sushanth_r@93233f36-2c2b-41c2-b1a5-e0ee276c51f9>
2011-09-12 00:24:22 +0000

    Improve scroll bounds estimator to handle document change better. Make scroll bounds estimator periodically update the scroll bounds.
    git-svn-id: https://subversion.assembla.com/svn/sushanth/trunk@59 93233f36-2c2b-41c2-b1a5-e0ee276c51f9

--- TextCore/TextCore/TextCoreControl/ScrollBoundsManager.cs
+++ TextCore/TextCore/TextCoreControl/ScrollBoundsManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Controls.Primitives;
-using System.Threading;
+using System.ComponentModel;
 
 using Microsoft.WindowsAPICodePack.DirectX.Controls;
 using Microsoft.WindowsAPICodePack.DirectX.DirectWrite;
@@ -12,6 +12,8 @@ namespace TextCoreControl
 {
     internal class ScrollBoundsManager
     {
+        const int UPDATE_INTERVAL = 100;
+
         public ScrollBoundsManager(ScrollBar vScrollBar, 
             ScrollBar hScrollBar, 
             DisplayManager displayManager, 
@@ -19,96 +21,175 @@ namespace TextCoreControl
             Document document)
         {
             this.totalLineCount = 0;
-            vScrollBar.Loaded += new System.Windows.RoutedEventHandler(vScrollBar_Loaded);
             this.vScrollBar = vScrollBar;
             this.hScrollBar = hScrollBar;
-            this.asyncScrollLengthEstimater = null;
             this.document = document;
-            renderHost.SizeChanged += new System.Windows.SizeChangedEventHandler(renderHost_SizeChanged);
             this.displayManager = displayManager;
+            this.textLayoutBuilder = new TextLayoutBuilder();
+            this.currentWidth = 0;
+            this.currentFirstVisibleOrdinal = Document.UNDEFINED_ORDINAL;
+
+            this.scrollLengthEstimator = new BackgroundWorker();
+            this.scrollLengthEstimator.WorkerReportsProgress = true;
+            this.scrollLengthEstimator.WorkerSupportsCancellation = true;
+            this.scrollLengthEstimator.DoWork += new DoWorkEventHandler(scrollLengthEstimator_DoWork);
+            this.scrollLengthEstimator.ProgressChanged += new ProgressChangedEventHandler(scrollLengthEstimator_ProgressChanged);
+            this.scrollLengthEstimator.RunWorkerCompleted += new RunWorkerCompletedEventHandler(scrollLengthEstimator_RunWorkerCompleted);
+
+            vScrollBar.Loaded += new System.Windows.RoutedEventHandler(vScrollBar_Loaded);
+            renderHost.SizeChanged += new System.Windows.SizeChangedEventHandler(renderHost_SizeChanged);
         }
 
         #region Event handler (Renderhost size change / loaded)
-        void renderHost_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
-        {
-            RenderHost renderHost = (RenderHost) sender;
-            this.InitializeVerticalScrollBounds((float)renderHost.ActualWidth);
-        }
 
         void vScrollBar_Loaded(object sender, System.Windows.RoutedEventArgs e)
         {
             DisableVScrollbar();
             DisableHScrollbar();
         }
+
+        void renderHost_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+            RenderHost renderHost = (RenderHost) sender;
+            this.InitializeVerticalScrollBounds(renderHost.ActualWidth);
+        }
+
+        internal void UpdateVerticalScrollBoundsDueToContentChange(int delta)
+        {
+            this.SetVerticalScrollBarLimits(this.totalLineCount + delta);
+
+            if (scrollLengthEstimator.IsBusy)
+            {
+                // Content changed, hence restart the scrollLengthEstimator if it has been running.
+                this.InitializeVerticalScrollBounds(this.currentWidth);
+            }
+        }
+
         #endregion
 
         #region Vertical scroll bounds estimation
-        public void InitializeVerticalScrollBounds(float width)
-        {
-            TextLayoutBuilder textLayoutBuilder = new TextLayoutBuilder();
-            object[] paramaterArray = { this.document, textLayoutBuilder, this, width, this.displayManager.FirstVisibleOrdinal()};
 
-            if (asyncScrollLengthEstimater != null)
+        public void InitializeVerticalScrollBounds(double width)
+        {
+            lock (this)
             {
-                asyncScrollLengthEstimater.Abort();
+                this.currentWidth = width;
+                this.currentFirstVisibleOrdinal = displayManager.FirstVisibleOrdinal();
             }
-            asyncScrollLengthEstimater = new Thread(AsyncScrollLengthEstimater);
-            asyncScrollLengthEstimater.Start(paramaterArray);
+
+            if (scrollLengthEstimator.IsBusy)
+            {
+                // Request a restart
+                scrollLengthEstimator.CancelAsync();
+            }
+            else
+            {
+                scrollLengthEstimator.RunWorkerAsync(this);
+            }
         }
 
-        static void AsyncScrollLengthEstimater(object paramaterArrayIn)
+        void scrollLengthEstimator_DoWork(object sender, DoWorkEventArgs e)
         {
-            object[] paramaterArray = (object[])paramaterArrayIn;
-            Document document = (Document)paramaterArray[0];
-            TextLayoutBuilder textLayoutBuilder = (TextLayoutBuilder)paramaterArray[1];
-            ScrollBoundsManager scrollBoundsManager = (ScrollBoundsManager)paramaterArray[2];
-            float layoutWidth = (float)paramaterArray[3];
-            int pageBeginOrdinal = (int)paramaterArray[4];
+            BackgroundWorker worker = sender as BackgroundWorker;
+            ScrollBoundsManager scrollBoundsManager = (ScrollBoundsManager)e.Argument;
+            e.Cancel = false;
+            e.Result = null;
 
-            int ordinal = document.FirstOrdinal();
-            int lineCount = 0;
-            int firstLineIndex = -1;
-            int newPageBeginOrdinal = 0;
-            double newPageTop = 0;
-
-            while (ordinal != Document.UNDEFINED_ORDINAL)
+            Document document = scrollBoundsManager.document;
+            float layoutWidth;
+            int firstVisibleOrdinal;
+            lock (scrollBoundsManager)
             {
-                VisualLine vl = textLayoutBuilder.GetNextLine(document, ordinal, layoutWidth, out ordinal);
-                if (firstLineIndex == -1)
+                layoutWidth = (float)scrollBoundsManager.currentWidth;
+                firstVisibleOrdinal = scrollBoundsManager.currentFirstVisibleOrdinal;
+            }
+
+            try
+            {
+                int ordinal = document.FirstOrdinal();
+                int lineCount = 0;
+                int firstLineIndex = -1;
+                int newPageBeginOrdinal = 0;
+                double newPageTop = 0;
+
+                while (ordinal != Document.UNDEFINED_ORDINAL)
                 {
-                    if (vl.NextOrdinal > pageBeginOrdinal)
+                    VisualLine vl = this.textLayoutBuilder.GetNextLine(document, ordinal, layoutWidth, out ordinal);
+                    if (firstLineIndex == -1)
                     {
-                        // page begin has been passed
-                        newPageBeginOrdinal = vl.BeginOrdinal;
-                        firstLineIndex = lineCount;
-                    }
-                    else
-                    {
-                        newPageTop += vl.Height;
-                    }
-                }
-                lineCount++;
-            }
-
-            scrollBoundsManager.InitializeVerticalScrollBounds(lineCount, newPageBeginOrdinal, newPageTop, firstLineIndex);
-        }
-
-        private void InitializeVerticalScrollBounds(int totalLineCount, int pageBeginOrdinal, double pageTop, double scrollOffset)
-        {
-            this.vScrollBar.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal,
-                new Action
-                (
-                    delegate()
-                    {
-                        this.SetVerticalScrollBarLimits(totalLineCount);
-                        if (this.vScrollBar.IsEnabled)
+                        if (vl.NextOrdinal > firstVisibleOrdinal)
                         {
-                            this.vScrollBar.Value = scrollOffset;
-                            this.displayManager.AdjustVScrollPositionForResize(pageBeginOrdinal, pageTop, scrollOffset);
+                            // page begin has been passed
+                            newPageBeginOrdinal = vl.BeginOrdinal;
+                            firstLineIndex = lineCount;
+                        }
+                        else
+                        {
+                            newPageTop += vl.Height;
                         }
                     }
-                )
-            );
+                    lineCount++;
+
+                    if (lineCount % UPDATE_INTERVAL == 0)
+                    {
+                        worker.ReportProgress(lineCount);
+                        if (worker.CancellationPending)
+                        {
+                            e.Cancel = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!e.Cancel)
+                {
+                    object[] resultArray = { lineCount, newPageBeginOrdinal, newPageTop, firstLineIndex };
+                    e.Result = resultArray;
+                }
+            }
+            catch
+            {
+                // Do nothing and let this thread rest in peace.
+                // It is legit to fall into this catch because document could have changed underneath us and
+                // we could try to access ordinals that dont exist. Simply ignore the failure as this
+                // thread will be restarted soon by the change notification.
+            }
+        }
+
+        void scrollLengthEstimator_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                // A restart was requested
+                scrollLengthEstimator.RunWorkerAsync(this);
+            }
+            else if (e.Error == null)
+            {
+                // We have a valid result.
+                object[] resultArray = (object []) e.Result;
+                int lineCount = (int)resultArray[0];
+                int newPageBeginOrdinal = (int)resultArray[1];
+                double newPageTop = (double)resultArray[2];
+                int firstLineIndex = (int)resultArray[3];
+
+                this.SetVerticalScrollBarLimits(lineCount);
+                if (this.vScrollBar.IsEnabled)
+                {
+                    this.vScrollBar.Value = firstLineIndex;
+                    this.displayManager.AdjustVScrollPositionForResize(newPageBeginOrdinal, newPageTop, firstLineIndex);
+                }
+            }
+        }
+
+        void scrollLengthEstimator_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            int totalLineCount = e.ProgressPercentage;
+            // To prevent jittery scroll thumb updates, update the thumb only if totalLineCount increases.
+            // On completion, we do update the thumb size to the final value correctly.
+            if (totalLineCount > this.totalLineCount)
+            {
+                this.SetVerticalScrollBarLimits(totalLineCount);
+            }
         }
 
         private void SetVerticalScrollBarLimits(int totalLineCount)
@@ -138,11 +219,6 @@ namespace TextCoreControl
                 // Vertical scrollbar not needed
                 DisableVScrollbar();
             }
-        }
-
-        internal void UpdateVerticalScrollBoundsDueToContentChange(int delta)
-        {
-            this.SetVerticalScrollBarLimits(this.totalLineCount + delta);
         }
 
         #endregion
@@ -231,10 +307,15 @@ namespace TextCoreControl
         #region Member data
         ScrollBar hScrollBar;
         ScrollBar vScrollBar;
-        Thread asyncScrollLengthEstimater;
+        BackgroundWorker scrollLengthEstimator;
         Document document;
         DisplayManager displayManager;
+        TextLayoutBuilder textLayoutBuilder;
         int totalLineCount;
+
+        double currentWidth;
+        int currentFirstVisibleOrdinal;
+
         #endregion
     }
 }
