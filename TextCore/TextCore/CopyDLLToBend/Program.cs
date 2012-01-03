de7ec6e5b8f5e194fc30be677fa460aba3f9803f
sushanth_r <sushanth_r@93233f36-2c2b-41c2-b1a5-e0ee276c51f9>
2012-01-03 00:44:07 +0000

    Add a utility project to copy files to Bend project
    git-svn-id: https://subversion.assembla.com/svn/sushanth/trunk@91 93233f36-2c2b-41c2-b1a5-e0ee276c51f9

---
+++ TextCore/TextCore/CopyDLLToBend/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace TransferToBend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.SetAttributes("D:\\Projects\\Bend\\Dll\\TextCoreControl.dll", FileAttributes.Normal);
+            File.Copy("TextCoreControl.dll", "D:\\Projects\\Bend\\Dll\\TextCoreControl.dll", true);
+            File.SetAttributes("D:\\Projects\\Bend\\Dll\\Microsoft.WindowsAPICodePack.DirectX.Controls.dll", FileAttributes.Normal);
+            File.Copy("Microsoft.WindowsAPICodePack.DirectX.Controls.dll", "D:\\Projects\\Bend\\Dll\\Microsoft.WindowsAPICodePack.DirectX.Controls.dll", true);
+            File.SetAttributes("D:\\Projects\\Bend\\Dll\\Microsoft.WindowsAPICodePack.DirectX.dll", FileAttributes.Normal);
+            File.Copy("Microsoft.WindowsAPICodePack.DirectX.dll", "D:\\Projects\\Bend\\Dll\\Microsoft.WindowsAPICodePack.DirectX.dll", true);
+        }
+    }
+}
---
+++ TextCore/TextCore/CopyDLLToBend/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CopyDLLToBend")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CopyDLLToBend")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bf7ff563-578b-4539-8192-b7cb1e60d78e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
